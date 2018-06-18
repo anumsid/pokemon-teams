92c76253a32693d356fc5bf7bd236b3b35aae840
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-06-18 00:16:00 -0400

    done

--- pokemon-teams-frontend/src/adapter.js
+++ pokemon-teams-frontend/src/adapter.js
@@ -1,21 +1,50 @@
+// const BASE_URL = "http://localhost:3000"
+// const TRAINERS_URL = `${BASE_URL}/trainers`
+// const POKEMONS_URL = `${BASE_URL}/pokemons`
+//
+// const Adapter = {
+//
+//  getAll: function getAll() {
+//    return fetch(TRAINERS_URL)
+//    .then((resp) => resp.json())
+//  },
+//
+//  createOne: function createOne(data) {
+//    return fetch(POKEMONS_URL, {
+//      method: 'POST'.
+//      headers: {
+//        'Content-Type': 'application/json'
+//      },
+//      body: JSON.stringify(data)
+//    }).then((resp) => resp.json())
+//  },
+//
+//
+//  deleteOne: function deleteOne(id) {
+//    return fetch(`${POKEMONS_URL}/${id}`, {
+//      method: 'DELETE'
+//    }).then((resp) => resp.json())
+//  }
+// }
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
 const Adapter = {
 
  getAll: function getAll() {
  return fetch(TRAINERS_URL)
- .then(response => response.json())
+ .then ((response) => response.json())
  },
 
  createOne: function createOne(data) {
  return fetch(POKEMONS_URL, {
-   method: 'POST',
-   headers: {
-     'Content-Type': 'application/json'
-   },
    body: JSON.stringify(data),
- })
+   headers: {
+     'content-type': 'application/json'
+   },
+   method: 'POST'
+   })
  .then(response => response.json())
  },
 
@@ -24,13 +53,5 @@ const Adapter = {
      method: 'DELETE'
    })
    .then(response => response.json())
-
-}
-
-
-
-
-
-
-
+ }
 }
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,49 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
- const main = document.querySelector('main');
- // const addButton = document.querySelectorAll('.addButton');
-
- renderCards();
+document.addEventListener('DOMContentLoaded', function() {
+  const main = document.getElementById("main");
+  main.addEventListener('click', (e) => {
+    if (e.target.className === 'addButton'){
+      const trainerId = e.target.dataset.trainerId;
+      Adapter.createOne({trainer_id: trainerId})
+      .then(function(){
+        main.innerHTML = '';
+        renderCards()
+      });
+    };
+    if (e.target.className === 'release'){
+      const pokemonId = e.target.dataset.pokemonId
+      Adapter.deleteOne(pokemonId)
+      .then(function(){
+        main.innerHTML = '';
+        renderCards()
+      });
+    };
+  });
+  renderCards()
 });
 
 
-
-
-function renderCards() {
- Adapter.getAll()
- .then((cards) => cards.forEach(renderCard));
+function renderCards(){
+  Adapter.getAll().then((cards) => cards.forEach(renderCard));
 }
 
 function renderCard(card) {
+  main.innerHTML +=
+  `<div class="card" data-id="${card.id}"<p>${card.name}</p>
+  <button class="addButton" data-trainer-id=${card.id}>Add Pokemon</button>
+  <ul>
+    ${renderCardPokemons(card)}
+  </ul>
 
-   card.addEventListener('click', handleClick)
-
-   main.innerHTML +=
-
- `<div class="card" data-id="${card.id}"><p>${card.name}</p>
-   <button class="addButton" data-trainer-id="${card.id}">Add Pokemon</button>
-   <ul>
-      ${renderCardPokemons(card)}
-   </ul>
- </div>`;
- //addButton = document.querySelectorAll('.addButton');
+  </div>`
 }
 
-
-
-function renderCardPokemons(card) {
- let ul = ''
-
- card.pokemons.forEach((pokemon) => {
-   ul += `<li>${pokemon.species} (${pokemon.nickname})<button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>`
- });
- return ul;
-}
-
-function handleClick(e) {
-  if(e.target.className === "addButton") {
-    Adapter.createOne(data)
-  }
-  if(e.target.className === "release"){
-    Adapter.deleteOne(e.target.parentElement.dataset.pokemon-id).then(e.target.parentElement.dataset.pokemon-id.remove())
-  }
+function renderCardPokemons(card){
+  let ul = ''
+  card.pokemons.forEach((pokemon) => {
+    ul += `<li>${pokemon.species} (${pokemon.nickname}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>`
+  })
+  return ul;
 }
